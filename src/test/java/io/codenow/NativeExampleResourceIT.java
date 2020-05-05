ae3c082cd2fb88582b8e2f20bad28637bfceadec
quarkusio <no-reply@quarkus.io>
2020-05-05 01:21:19 +0000

    Initial commit

---
+++ src/test/java/io/codenow/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package io.codenow;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
